1ed1a8c5ffc8277523009d131fd159b40f7700fd
Jangsuk Ryoo <jangsukr@gmail.com>
2022-07-17 23:45:50 +0900

    인풋 필드

--- test1/src/views/HomeView.vue
+++ test1/src/views/HomeView.vue
@@ -1,25 +1,40 @@
 <template>
   <div>
     <h1>"This is Home Page" </h1>
-    <JSR title="home title"/>
-    <JSR />>
+    <!-- <JSR title="home title"/>
+    <JSR />> -->
+    <form action="">
+      <InputField v-model="name" />
+      <br><button>Submit</button>
+    </form>
+    {{ name }}
   </div>
 </template>
 
 <script>
-import JSR from '@/components/JSR.vue';
+import InputField from '../components/InputField.vue'
+// import JSR from '@/components/JSR.vue';
 
 export default{
-  components:{
-    JSR
-  },
+  components: { InputField }, 
+  // components:{
+  //   JSR
+  // },
   data(){
     return{
-      name: 'JSR'
+      // name: 'JSR'
+      name:''
     }
-  }
+  },
+  // methods: {
+  //   updateName(name){
+  //     this.name = name;
+  //   }
+  // }
 }
 </script>
+
+
 
 <style scoped>
 h1 {
