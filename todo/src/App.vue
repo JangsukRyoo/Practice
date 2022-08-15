97728132a99c3841507063bcadb86b0547f1545a
Jangsuk Ryoo <jangsukr@gmail.com>
2022-08-15 22:12:21 +0900

    TodoList Test

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="app" class ="container">
     <h1 class ="text-center"> Todo App</h1>
-    <input type="text" class="w-100"> 
+    <input type="text" 
+    class="w-100 p-2" p
+    laceholder="Type todo"
+    @keyup.enter="addTodo"> 
     <hr>
-    <div>
-      <input type="checkbox">
-      <span class="ml-3">Buy a car</span>
-    </div>    
+  <todo-list v-for="todo in todos" :key="todo.id" :todo="todo"/>
+
   </div>
 </template>
 
 <script>
 
-
+import TodoList from './components/TodoList.vue';
+ 
 export default {
+  components: { TodoList },
+  data(){
+    return{
+      todos:[
+      { id:1, text: "buy a car", checked:false}, 
+      { id:2, text: "play game", checked:false},
+      ]
+     }
+  },
 
-  
+  methods: {
+    addTodo(e){
+      this.todos.push(
+        {
+        id: Math.random(),
+        text:e.target.value,
+        checked: false
+        })
+    }
+  }
 }
 </script>
 <style>
---
+++ todo/src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+      <input type="checkbox" :cheked="todo.checked">
+      <span class="ml-3">{{ todo.text}}</span>
+    </div>    
+
+</template>
+
+<script>
+export default {
+  props:{
+    todo: {
+      type:Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+ 
+</style>
