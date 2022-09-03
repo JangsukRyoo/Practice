b5734e0b86bedf7f17d5c4af04fc247d8f96814c
Jangsuk Ryoo <jangsukr@gmail.com>
2022-09-03 23:52:37 +0900

    Update App.vue

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -60,7 +60,6 @@ export default {
 
   <script>
       var tag = document.createElement('script');
-
       tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
