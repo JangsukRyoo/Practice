b97f28ee5997c6f1c5d718926901ecbadf95554f
Jangsuk Ryoo <jangsukr@gmail.com>
2022-10-11 00:47:06 +0900

    PDF TEST

---
+++ pdf/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>File</h1>
+    <vue-pdf-embed :source="source1" />
+  </div>
+</template>
+
+<script>
+import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
+
+export default {
+  components: {
+    VuePdfEmbed,
+  },
+  data() {
+    return {
+      source1: 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf',
+    }
+  },
+}
+</script>
